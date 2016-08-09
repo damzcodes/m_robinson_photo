569a95861065b839c866c3174d1e27532192fab7
damzcodes <damzcodes@gmail.com>
2016-08-09 19:09:08 +0100

    Admin can create public pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705134555) do
+ActiveRecord::Schema.define(version: 20160809174502) do
+
+  create_table "galleries", force: :cascade do |t|
+    t.string   "title"
+    t.string   "slug"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
